b1702dcd8e7a814610a71ee15e02051e63057094
KC Ikegwu <ikegwukc@gmail.com>
2015-02-23 20:19:58 -0500

    The following directory contains an example environment, and sample game objects and scripts
    Game Objects are in: /test02/Assets/LeapMotion/Prefabs
    
    Scripts are in: /test02/Assets/LeapMotion/Scripts
    
    See HandController.cs and Sample.cs
    
    The setup scene is in the root directory of Assets and is called “basic
    hand scene.unity”

---
+++ test02/Assets/LeapMotion/Scripts/Sample.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+/*public class Sample : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}*/
+
+class Sample : MonoBehaviour
+{
+	public static void Main ()
+	{
+		SampleListener listener = new SampleListener ();
+		Controller controller = new Controller ();
+		controller.AddListener (listener);
+		
+		// Keep this process running until Enter is pressed
+		Debug.Log ("Press Enter to quit...");
+		Debug.Log ("");
+		
+		controller.RemoveListener (listener);
+		controller.Dispose ();
+	}
+}
+
+class SampleListener : Listener
+{
+	private Object thisLock = new Object ();
+	
+	private void SafeWriteLine (string line)
+	{
+		lock (thisLock) {
+			Debug.Log ("");
+			//Console.WriteLine (line);
+		}
+	}
+	
+	public override void OnConnect (Controller controller)
+	{
+		SafeWriteLine ("Connected");
+	}
+	
+	
+	public override void OnFrame (Controller controller)
+	{
+		SafeWriteLine ("Frame available");
+	}
+}
---
+++ test02/Assets/rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Quaternion kc = new Quaternion(0,0,0,0);
+		transform.rotation = new Quaternion(0,0,0,0);
+		transform.Rotate (new Vector3 (0, 0, Input.mousePosition.y));
+	}
+}
