206828af0416f1ca7d3ed8b129ed398beed3b434
KC Ikegwu <ikegwukc@gmail.com>
2015-03-02 16:37:30 -0500

    Example of how to use LP in Unity
    hands_example.cs rotates a Cube based on the rotation of your hand.
    
    gesture example.cs rotates a Cube based on using  gesture called swipe.
    When you swipe your hand to the left, the object rotates to left.

---
+++ learning/test02/Assets/gesture_example.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+class gesture_example : MonoBehaviour
+{
+	Controller controller;
+	public GameObject cube_obj;
+	public bool spin;
+	// Use this for initialization
+	void Start () {
+		SampleListener listener = new SampleListener ();
+		controller = new Controller ();
+		controller.EnableGesture (Gesture.GestureType.TYPESWIPE);
+		controller.Config.SetFloat ("Gesture.Swipe.MinLength", 200.0f);
+		controller.Config.SetFloat ("Gesture.Swipe.MinVelocity", 750f);
+		controller.Config.Save ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frame frame = controller.Frame ();
+		GestureList gestures = frame.Gestures ();
+		Hand hand = frame.Hands.Frontmost;
+		if (hand.IsRight) {
+			for(int i=0; i<gestures.Count;i++){
+				Gesture gesture =gestures[i];
+				spin=false;
+				if(gesture.Type == Gesture.GestureType.TYPESWIPE){
+					SwipeGesture Swipe = new SwipeGesture(gesture);
+					Vector swipeDirection = Swipe.Direction;
+					if(swipeDirection.x<0){
+						Debug.Log("Left");
+						spin=true;
+					}
+				}
+			}
+		}
+		if(spin==true){
+			cube_obj.transform.Rotate(Vector3.up, 45 * Time.deltaTime);
+		}
+	}
+}
---
+++ learning/test02/Assets/hands_example.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+class hands_example : MonoBehaviour
+{
+	Controller controller; //  Creates a virtual controller object, we interact with leap motion this way.
+	public GameObject cube_obj;
+
+	// Use this for initialization
+	void Start () {
+		controller = new Controller ();
+		controller.Config.Save ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Frame frame = controller.Frame (); //Frames kind of like a list or array that contains all of the tracking data in time.
+		Hand hand = frame.Hands.Frontmost; // Find the Hand most torward the front, you can change this.
+		if (hand.IsRight) { // Just doing stuff with the Right Hand you certainly can change this to left or both, etc.
+
+			//Debug.Log ("Pitch: "+ hand.Direction.Pitch); // Gives the rotational value of Hand as a float with respect to the x axis.
+			//Debug.Log ("Yaw: " + hand.Direction.Yaw); // Gives the rotational value of Hand as a float with respect to the y axis.
+			Debug.Log ("Roll: " + (-1*hand.Direction.Roll)); // Gives the rotational value of Hand as a float with respect to the z axis.
+
+			cube_obj.transform.Rotate(0,0,hand.Direction.Roll);//Rotate game object with the rotation of the hand.
+		}
+	}
+}
